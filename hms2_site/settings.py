4d961f601006e756f51d4586301dfa07d1e12c3b
Scott <scottc520@gmail.com>
2020-03-22 10:59:58 -0600

    Moved CORS from MIDDLEWARE_CLASSES to MIDDLEWARE

--- hms2_site/settings.py
+++ hms2_site/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,11 +55,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-)
+# MIDDLEWARE_CLASSES = (
+#     'corsheaders.middleware.CorsMiddleware',
+#     'django.middleware.common.BrokenLinkEmailsMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+# )
 
 CORS_ORIGIN_ALLOW_ALL = True
 
